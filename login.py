c691bb880fb0fb9389f11ea363ac88da77fc09be
Charles George <cgmac@MacBook-Pro.local>
2021-12-15 23:23:39 -0500

    Added Login variables and validation

--- login.py
+++ login.py
@@ -7,10 +7,6 @@ from getpass import getpass
 
 
 def main():
-    """
-
-    :return:
-    """
     password = getpass(prompt='Password: ', stream=None)
     if run_method == "headless":
         def set_chrome_options():
@@ -44,6 +40,30 @@ def main():
 
         chrome_options = set_chrome_options()
         driver = webdriver.Chrome(options=chrome_options)
+
+        # TODO: Identify Login URL
+        driver.get("")
+
+        # TODO: Identify login fields Class Names and Login Button id name
+        driver.find_element(By.CLASS_NAME, "").send_keys(username)
+        driver.find_element(By.CLASS_NAME, "").send_keys(password)
+        driver.find_element(By.ID, "").click()
+
+        # Wait for the ready state to be complete
+        WebDriverWait(driver= driver, timeout=10).until(
+            lambda x: x.execute_script("return document.readState === 'complete'")
+        )
+
+        # Verify Login Success
+        error_message = "Incorrect username or password."
+        errors = driver.find_element(By.CLASS_NAME, "flash-error")
+        for e in errors:
+            print(e.text)
+        if any(error_message in e.text for e in errors):
+            print("[!] Login Failed")
+        else:
+            print("[+] Login Successful")
+
 
 if __name__ == '__main__':
     main()
